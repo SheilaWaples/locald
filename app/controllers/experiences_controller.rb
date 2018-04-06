c1cd195a96303c82d1a6106280984f72c87031fb
SheilaWaples <sheilawaples78@gmail.com>
2018-04-06 16:11:47 -0400

    final

--- app/controllers/experiences_controller.rb
+++ app/controllers/experiences_controller.rb
@@ -72,5 +72,7 @@ class ExperiencesController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def experience_params
       params.require(:experience).permit(:name, :description, :price, :image, :user_id)
+
     end
 end
+
